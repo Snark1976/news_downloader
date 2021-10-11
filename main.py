a9981d4876bf3dfaa98b1f3536593e9227565628
Snark1976 <kohovec@gmail.com>
2021-10-11 17:04:16 +0300

    Added logging

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, DateTime, ForeignKey, \
-    Text, insert, select
+    Text, insert, select, func
 from sqlalchemy.exc import SQLAlchemyError
-from requests.exceptions import RequestException
 from datetime import datetime
 import logging
 import parsers
@@ -42,9 +41,9 @@ news = Table('news', metadata,
 # Compare the list of resources in the database (tables "sources" and "source_urls") and in the parser module
 # (parsers.py). Add new resources to the database.
 def check_resource_list():
-    try:
-        list_url_sources = [i[0] for i in db_news.execute(select([sources.c.url])).fetchall()]
-        for resource in parsers.list_sources:
+    list_url_sources = [i[0] for i in db_news.execute(select([sources.c.url])).fetchall()]
+    for resource in parsers.list_sources:
+        try:
             if resource['url'] not in list_url_sources:
                 db_news.execute(insert(sources),
                                 name=resource['name'],
@@ -55,15 +54,19 @@ def check_resource_list():
                     db_news.execute(insert(source_urls),
                                     source_id=get_source_id(resource['url']),
                                     url=link)
-    except SQLAlchemyError:
-        pass
+        except SQLAlchemyError:
+            logging.exception(f'Error adding source to database: {resource}')
 
 
 # Getting id from table "sources" by url value
 def get_source_id(url):
-    sel = select([sources]).where(sources.c.url == url)
-    source_id = db_news.execute(sel).scalar()
-    return source_id
+    try:
+        sel = select([sources]).where(sources.c.url == url)
+        source_id = db_news.execute(sel).scalar()
+        return source_id
+
+    except SQLAlchemyError:
+        logging.exception(f'Error getting the source ID. Link: {url}')
 
 
 # Loading a list of fresh news from a source
@@ -72,32 +75,26 @@ def download_news(data_source):
     func_parser = data_source['func_parser']
     func_checking_news = data_source['func_checking']
     urls_source = data_source['links_of_parse']
-    source_id = get_source_id(data_source['url'])
 
-    try:
-        list_news = func_parser(*urls_source)
+    list_news = func_parser(*urls_source)
+    if source_id := get_source_id(data_source['url']):
         for news_ in list_news:
-            try:
-                if is_fresh_news(news_):
-                    news_['source_id'] = source_id
-                    result.append(func_checking_news(news_))
-
-            except RequestException:
-                pass
-            except SQLAlchemyError:
-                pass
-
-    except RequestException:
-        pass
-
+            if is_fresh_news(news_):
+                news_['source_id'] = source_id
+                result.append(func_checking_news(news_))
     return result
 
 
 # Checking the presence of this news in the database
 def is_fresh_news(this_news):
-    sel = select([news.c.link]).where(news.c.link == this_news['link'])
-    result = db_news.execute(sel).scalar()
-    return not result
+    try:
+        sel = select([news.c.link]).where(news.c.link == this_news['link'])
+        result = db_news.execute(sel).scalar()
+        return not result
+
+    except SQLAlchemyError:
+        logging.exception(f'Error checking the presence of this news in the database: {this_news["link"]}')
+        return False
 
 
 def add_news_to_database(n_list):
@@ -105,16 +102,23 @@ def add_news_to_database(n_list):
         try:
             db_news.execute(insert(news), news_)
         except SQLAlchemyError:
-            print('Error', news_)
+            logging.exception(f'Error adding news to database: {news_}')
+
+
+def get_count_news():
+    sel = select([func.count()]).select_from(news)
+    result = db_news.execute(sel).scalar()
+    return result
 
 
 if __name__ == "__main__":
-    logging.info(f' Time: {datetime.now()}. Start loading news.')
     metadata.create_all(engine)
+    logging.info(f' Time: {datetime.now()}. Start loading news.')
     check_resource_list()
+    count_news = get_count_news()
     news_list = []
     for source in parsers.list_sources:
         news_list.extend(download_news(source))
     news_list.sort(key=lambda x: x['datetime'])
     add_news_to_database(news_list)
-    logging.info(f' Time: {datetime.now()}. Stop loading news. Add news to DB: {len(news_list)}')
+    logging.info(f' Time: {datetime.now()}. Stop loading news. Added news to DB: {get_count_news() - count_news}')
--- parsers.py
+++ parsers.py
@@ -2,199 +2,239 @@ import requests
 import maya
 from bs4 import BeautifulSoup
 from datetime import datetime, timezone
+from requests.exceptions import RequestException
+import logging
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                          'AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/94.0.4606.61 '
                          'Safari/537.36'}
 
+logging.basicConfig(filename="timeline.log", level=logging.INFO)
+
+
+def get_page(url):
+    try:
+        page = requests.get(url, headers=headers)
+
+        if page.status_code == 200:
+            return page
+        else:
+            logging.error(f'Page loading error. Status code: {page.status_code}, link: {url}.')
+
+    except RequestException:
+        logging.exception(f'Page loading error. Link: {url}')
+
 
 def parser_n_plus_1(url):
     result = []
-    page = requests.get(url, headers=headers)
 
-    if page.status_code == 200:
+    if page := get_page(url):
         soup = BeautifulSoup(page.text, 'xml')
         all_news_this_source = soup.find_all('item')
 
         for news_this_source in all_news_this_source:
-            news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
-                    'title': news_this_source.title.text.replace('\xa0', ' '),
-                    'description': None,
-                    'link': news_this_source.link.text,
-                    'media': str(news_this_source.find('media:content')).split('"')[3],
-                    'tags': None
-                    }
-            result.append(news)
+            try:
+                news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
+                        'title': news_this_source.title.text.replace('\xa0', ' '),
+                        'description': None,
+                        'link': news_this_source.link.text,
+                        'media': str(news_this_source.find('media:content')).split('"')[3],
+                        'tags': None
+                        }
+                result.append(news)
+
+            except (LookupError, TypeError, AttributeError, NameError, ValueError):
+                logging.exception(f'Parsing error. Parsed text: \n{news_this_source}')
 
     return result
 
 
 def parser_dev_by(url):
     result = []
-    page = requests.get(url, headers=headers)
 
-    if page.status_code == 200:
+    if page := get_page(url):
         soup = BeautifulSoup(page.text, 'xml')
         all_news_this_source = soup.find_all('item')
 
         for news_this_source in all_news_this_source:
-            news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
-                    'title': news_this_source.title.text.replace('\xa0', ' '),
-                    'description': news_this_source.description.text.strip().replace('\xa0', ' '),
-                    'link': news_this_source.link.text,
-                    'media': str(news_this_source.find('enclosure')).split('"')[3],
-                    'tags': None
-                    }
-            result.append(news)
+            try:
+                news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
+                        'title': news_this_source.title.text.replace('\xa0', ' '),
+                        'description': news_this_source.description.text.strip().replace('\xa0', ' '),
+                        'link': news_this_source.link.text,
+                        'media': str(news_this_source.find('enclosure')).split('"')[3],
+                        'tags': None
+                        }
+                result.append(news)
+
+            except (LookupError, TypeError, AttributeError, NameError, ValueError):
+                logging.exception(f'Parsing error. Parsed text: \n{news_this_source}')
 
     return result
 
 
 def parser_bbc_russian(url):
     result = []
-    page = requests.get(url, headers=headers)
 
-    if page.status_code == 200:
+    if page := get_page(url):
         soup = BeautifulSoup(page.text, 'xml')
         all_news_this_source = soup.find_all('item')
 
         for news_this_source in all_news_this_source:
-            news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
-                    'title': news_this_source.title.text.replace('\xa0', ' '),
-                    'description': news_this_source.description.text.strip().replace('\xa0', ' '),
-                    'link': news_this_source.link.text,
-                    'media': None,
-                    'tags': None
-                    }
-            result.append(news)
+            try:
+                news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
+                        'title': news_this_source.title.text.replace('\xa0', ' '),
+                        'description': news_this_source.description.text.strip().replace('\xa0', ' '),
+                        'link': news_this_source.link.text,
+                        'media': None,
+                        'tags': None
+                        }
+                result.append(news)
+
+            except (LookupError, TypeError, AttributeError, NameError, ValueError):
+                logging.exception(f'Parsing error. Parsed text: \n{news_this_source}')
 
     return result
 
 
 def parser_deutsche_welle(url):
     result = []
-    page = requests.get(url, headers=headers)
 
-    if page.status_code == 200:
+    if page := get_page(url):
         soup = BeautifulSoup(page.text, 'xml')
         all_news_this_source = soup.find_all('item')
 
         for news_this_source in all_news_this_source:
-            news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
-                    'title': news_this_source.title.text.replace('\xa0', ' '),
-                    'description': news_this_source.description.text.strip().replace('\xa0', ' '),
-                    'link': news_this_source.link.text,
-                    'media': None,
-                    'tags': None
-                    }
-            result.append(news)
+            try:
+                news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
+                        'link': news_this_source.link.text,
+                        'title': news_this_source.title.text.replace('\xa0', ' '),
+                        'description': news_this_source.description.text.strip().replace('\xa0', ' '),
+                        'media': None,
+                        'tags': None
+                        }
+                result.append(news)
+
+            except (LookupError, TypeError, AttributeError, NameError, ValueError):
+                logging.exception(f'Parsing error. Parsed text: \n{news_this_source}')
 
     return result
 
 
 def parser_lenta_ru(url):
     result = []
-    page = requests.get(url, headers=headers)
 
-    if page.status_code == 200:
+    if page := get_page(url):
         soup = BeautifulSoup(page.text, 'xml')
         all_news_this_source = soup.find_all('item')
 
         for news_this_source in all_news_this_source:
-            news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
-                    'title': news_this_source.title.text.replace('\xa0', ' '),
-                    'description': news_this_source.description.text.strip().replace('\xa0', ' '),
-                    'link': news_this_source.link.text,
-                    'media': str(news_this_source.find('enclosure')).split('"')[5],
-                    'tags': news_this_source.category.text
-                    }
-            result.append(news)
+            try:
+                news = {'datetime': maya.parse(news_this_source.pubDate.text).datetime(),
+                        'title': news_this_source.title.text.replace('\xa0', ' '),
+                        'description': news_this_source.description.text.strip().replace('\xa0', ' '),
+                        'link': news_this_source.link.text,
+                        'media': str(news_this_source.find('enclosure')).split('"')[5],
+                        'tags': news_this_source.category.text
+                        }
+                result.append(news)
+
+            except (LookupError, TypeError, AttributeError, NameError, ValueError):
+                logging.exception(f'Parsing error. Parsed text: \n{news_this_source}')
 
     return result
 
 
-def parser_century22(*url):
+def parser_century22(*urls):
     result = []
     all_news_this_source = []
 
-    page = requests.get(url[0], headers=headers)
-
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.text, 'lxml')
-        all_news_this_source.append(soup.find('article', class_='article-item article-item-3_4'))
-        all_news_this_source.extend(soup.find_all('article', class_='article-item article-item-1_2'))
-
-    page = requests.get(url[1], headers=headers)
-
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.text, 'lxml')
-        all_news_this_source.append(soup.find('article', class_='article-item article-item-3_4'))
-        all_news_this_source.extend(soup.find_all('article', class_='article-item article-item-1_2'))
+    for url in urls:
+        if page := get_page(url):
+            soup = BeautifulSoup(page.text, 'lxml')
+            all_news_this_source.append(soup.find('article', class_='article-item article-item-3_4'))
+            all_news_this_source.extend(soup.find_all('article', class_='article-item article-item-1_2'))
 
     for news_this_source in all_news_this_source:
-        date = maya.parse(news_this_source.select('time')[0]['datatime']).datetime()
-        date = datetime.utcnow() if datetime.now().date() == date.date() else date
-        news = {'datetime': date.replace(second=0, microsecond=0, tzinfo=timezone.utc),
-                'title': news_this_source.select('h3.item_link a')[0].text.strip().replace('\xa0', ' '),
-                'description': None,
-                'link': news_this_source.select('h3.item_link a')[0]['href'],
-                'media': news_this_source.select('img')[0]['src'] if news_this_source.select('img') else None,
-                'tags': None
-                }
-        result.append(news)
+        try:
+            date = maya.parse(news_this_source.select('time')[0]['datatime']).datetime()
+            date = datetime.utcnow() if datetime.now().date() == date.date() else date
+            news = {'datetime': date.replace(second=0, microsecond=0, tzinfo=timezone.utc),
+                    'title': news_this_source.select('h3.item_link a')[0].text.strip().replace('\xa0', ' '),
+                    'description': None,
+                    'link': news_this_source.select('h3.item_link a')[0]['href'],
+                    'media': news_this_source.select('img')[0]['src'] if news_this_source.select('img') else None,
+                    'tags': None
+                    }
+            result.append(news)
+
+        except (LookupError, TypeError, AttributeError, NameError, ValueError):
+            logging.exception(f'Parsing error. Parsed text: \n{news_this_source}')
 
     return result
 
 
 def parser_n_plus_1_news_checking(news):
-    page = requests.get(news['link'], headers=headers)
 
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.text, 'lxml')
-        tags = soup.find_all('a', attrs={'data-rubric': True})
-        news['tags'] = ', '.join(tag.text for tag in tags)
-        for paragraph in soup.select('div.body.js-mediator-article p'):
-            if len(paragraph.text) > 100:
-                news['description'] = paragraph.text.strip().replace('\xa0', ' ').replace('\n', ' ')
-                break
+    if page := get_page(news['link']):
+        try:
+            soup = BeautifulSoup(page.text, 'lxml')
+            tags = soup.find_all('a', attrs={'data-rubric': True})
+            news['tags'] = ', '.join(tag.text for tag in tags)
+            for paragraph in soup.select('div.body.js-mediator-article p'):
+                if len(paragraph.text) > 100:
+                    news['description'] = paragraph.text.strip().replace('\xa0', ' ').replace('\n', ' ')
+                    break
+
+        except (LookupError, TypeError, AttributeError, NameError, ValueError):
+            logging.exception(f'Parsing error. Parsed page: \n{news["link"]}')
 
     return news
 
 
 def parser_dev_by_news_checking(news):
-    page = requests.get(news['link'], headers=headers)
 
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.text, 'lxml')
-        news['tags'] = [st.text
-                        for st in soup.select('span.article-meta__item')
-                        if 'Тег' in st.text
-                        ][0].replace('Теги: ', '')
+    if page := get_page(news['link']):
+        try:
+            soup = BeautifulSoup(page.text, 'lxml')
+            news['tags'] = [st.text
+                            for st in soup.select('span.article-meta__item')
+                            if 'Тег' in st.text
+                            ][0].replace('Теги: ', '')
+
+        except (LookupError, TypeError, AttributeError, NameError, ValueError):
+            logging.exception(f'Parsing error. Parsed page: \n{news["link"]}')
 
     return news
 
 
 def parser_bbc_russian_news_checking(news):
-    page = requests.get(news['link'], headers=headers)
 
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.text, 'lxml')
-        meta_tags = soup.find_all('meta', attrs={'name': "article:tag"})
-        news['tags'] = ', '.join(meta['content'] for meta in meta_tags)
-        news['media'] = soup.select('div figure div img')[0]['src'] if soup.select('div figure div img') else None
+    if page := get_page(news['link']):
+        try:
+            soup = BeautifulSoup(page.text, 'lxml')
+            meta_tags = soup.find_all('meta', attrs={'name': "article:tag"})
+            news['tags'] = ', '.join(meta['content'] for meta in meta_tags)
+            news['media'] = soup.select('div figure div img')[0]['src'] if soup.select('div figure div img') else None
+
+        except (LookupError, TypeError, AttributeError, NameError, ValueError):
+            logging.exception(f'Parsing error. Parsed page: \n{news["link"]}')
 
     return news
 
 
 def parser_deutsche_welle_news_checking(news):
-    page = requests.get(news['link'], headers=headers)
 
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.text, 'lxml')
-        tags = soup.select('div ul.smallList li')[2]
-        news['tags'] = ''.join(tag.text for tag in tags if 'Темы' not in tag.text).strip()
-        news['media'] = soup.select('div a img')[0]['src'] if soup.select('div a img') else None
+    if page := get_page(news['link']):
+        try:
+            soup = BeautifulSoup(page.text, 'lxml')
+            tags = soup.select('div ul.smallList li')[2]
+            news['tags'] = ''.join(tag.text for tag in tags if 'Темы' not in tag.text).strip()
+            news['media'] = soup.select('div a img')[0]['src'] if soup.select('div a img') else None
+
+        except (LookupError, TypeError, AttributeError, NameError, ValueError):
+            logging.exception(f'Parsing error. Parsed page: \n{news["link"]}')
 
     return news
 
@@ -205,13 +245,16 @@ def parser_lenta_ru_news_checking(news):
 
 
 def parser_century22_news_checking(news):
-    page = requests.get(news['link'], headers=headers)
 
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.text, 'lxml')
-        tags = soup.select('div.content_column_footer div.page_tags a')
-        news['tags'] = '. '.join(tag.text.strip() for tag in tags)
-        news['description'] = soup.find('p', class_='text_strong').text.replace('\xa0', ' ')
+    if page := get_page(news['link']):
+        try:
+            soup = BeautifulSoup(page.text, 'lxml')
+            tags = soup.select('div.content_column_footer div.page_tags a')
+            news['tags'] = '. '.join(tag.text.strip() for tag in tags)
+            news['description'] = soup.find('p', class_='text_strong').text.replace('\xa0', ' ')
+
+        except (LookupError, TypeError, AttributeError, NameError, ValueError):
+            logging.exception(f'Parsing error. Parsed page: \n{news["link"]}')
 
     return news
 
@@ -266,10 +309,5 @@ if __name__ == "__main__":
     all_news = []
     for parser in list_sources:
         all_news.extend(parser['func_parser'](*parser['links_of_parse']))
-    #
-    # print(*sorted(all_news, key=lambda x: x['datetime'], reverse=True), len(all_news), sep='\n')
-    # parser = list_sources[0]
-    # news = parser['func_parser'](*parser['links_of_parse'])
-    # for nws in news:
-    #     result = parser['func_checking'](nws)
-    #     print(result)
+
+    print(*sorted(all_news, key=lambda x: x['datetime'], reverse=True), len(all_news), sep='\n')
