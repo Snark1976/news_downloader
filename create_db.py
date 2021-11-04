39472f5e8929247b931ce16f530a52cdb862ba36
Snark1976 <kohovec@gmail.com>
2021-11-04 14:12:52 +0300

    changing config.py

--- create_db.py
+++ create_db.py
@@ -1,8 +1,9 @@
 import pymysql
+from config import *
 
 
-connection = pymysql.connect(host='localhost', user='root', password='Colobuc')
+connection = pymysql.connect(host=host_db, user=name_user_db, password=password_user_db)
 
 with connection:
     cursor = connection.cursor()
-    cursor.execute('CREATE DATABASE db_news')
+    cursor.execute(f'CREATE DATABASE {name_db}')
